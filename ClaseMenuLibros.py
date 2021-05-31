cab45331824fb831de507c5eedf566d5199c2a47
cirodelgado7 <82001073+cirodelgado7@users.noreply.github.com>
2021-05-31 02:20:17 -0300

    Add files via upload

---
+++ ClaseMenuLibros.py
@@ -0,0 +1,33 @@
+class Menu:
+    __switcher = None
+
+    def __init__(self):
+        self.__switcher = {
+            1: self.opcion1,
+            2: self.opcion2,
+            3: self.salir
+        }
+
+    def getSwitcher(self):
+        return self.__switcher
+
+    def opcion(self, op, libros):
+        func = self.__switcher.get(op, lambda: print("Opción no válida"))
+        func(libros)
+
+    def opcion1(self, libros):
+        id = int(input('Ingrese el id del libro: '))
+        if id == 10001:
+            libros.mostrarDatos(id)
+        elif id == 10002:
+            libros.mostrarDatos(id)
+        else:
+            print('Identificador de libro no encontrado')
+
+    def opcion2(self, libros):
+        palabra = input("Ingrese palabra a Buscar: ")
+        for libros in libros.buscarPalabra(palabra):
+            print("\nTitulo: {} \nAutor: {}".format(libros.getTitulo(), libros.getAutor()))
+
+    def salir(self, libros):
+        print('Salir')
---
+++ MainLibros.py
@@ -0,0 +1,20 @@
+from manejadorLibros import manejadorLibros
+from ClaseMenuLibros import Menu
+
+if __name__ == '__main__':
+    libros = manejadorLibros()
+    libros.cargarDatos()
+    print(libros)
+    menu = Menu()
+    salir = False
+    while not salir:
+        print("\n------------Menu------------\n"
+              "1. Inciso 1"
+              "\n2. Inciso 2"
+              "\n3. Salir")
+        op = int(input('Ingrese una opcion: '))
+        if op in range(1, 4) and type(op) is not str:
+            menu.opcion(op, libros)
+            salir = op == 3
+        else:
+            print('La opción ingresada no es valida. Ingrese una opción válida')
